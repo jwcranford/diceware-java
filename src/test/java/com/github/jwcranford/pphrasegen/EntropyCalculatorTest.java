b198ef4238f6f1ca4e3f309fa3ab4d792ddfe73f
Jonathan W Cranford <jonathan.w.cranford@gmail.com>
2019-09-11 13:03:35 -0600

    Added unit test - WIP

---
+++ src/test/java/com/github/jwcranford/pphrasegen/EntropyCalculatorTest.java
@@ -0,0 +1,22 @@
+package com.github.jwcranford.pphrasegen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EntropyCalculatorTest {
+
+  @Test
+  void testPowerOfTwo() {
+    final EntropyCalculator calc = new EntropyCalculator(1024);
+    assertEquals(1024, calc.getEffectiveWordListSize());
+    assertEquals(8, (int) Math.ceil(calc.calculateWordCount((double) 0)));
+  }
+
+  @Test
+  void testNotPowerOfTwo() {
+    final EntropyCalculator calc = new EntropyCalculator(1100);
+    assertEquals(1024, calc.getEffectiveWordListSize());
+    assertEquals(8, (int) Math.ceil(calc.calculateWordCount((double) 0)));
+  }
+}
